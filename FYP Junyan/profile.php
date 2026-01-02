1a0d71ff7fdd04ade4c117866e4f7abecd59895b
LIM JUN YAN <1231200684@student.mmu.edu.my>
2026-01-02 23:21:22 +0800

    test

--- FYP Junyan/profile.php
+++ FYP Junyan/profile.php
@@ -126,6 +126,26 @@ try {
         </div>
     </main>
 
+    <footer>
+        <div class="container">
+            <div class="footer-content">
+                <div class="footer-logo">
+                    <img src="Bakery House Logo.png" alt="BakeryHouse">
+                </div>
+                <p>Sweet & Delicious</p>
+                <div class="footer-links">
+                    <a href="mainpage.php">Home</a>
+                    <a href="menu.php">Menu</a>
+                    <a href="about_us.php">About</a>
+                    <a href="contact_us.php">Contact</a>
+                    <a href="privacypolicy.php">Privacy Policy</a>
+                    <a href="termservice.php">Terms of Service</a>
+                </div>
+                <p>&copy; 2024 BakeryHouse. All rights reserved.</p>
+            </div>
+        </div>
+    </footer>
+
     <script src="profile.js"></script>
 </body>
 </html>
